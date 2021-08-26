fe0ba18519e18290d8284caa63b40e1d1eb2c3b6
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 17:37:01 -0400

    refactoring

--- test/checkout_spec.js
+++ test/checkout_spec.js
@@ -5,6 +5,8 @@ let checkout;
 
 beforeEach(function(){
     checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
 });
 
 it('Can initiate a checkout class', function(){
@@ -12,7 +14,7 @@ it('Can initiate a checkout class', function(){
 });
 
 it('Can add item price: addItemPrice', function(){
-    checkout.addItemPrice('a', 1);
+    
 });
 
 it('Can add item: addItemToCart', function(){
@@ -20,14 +22,11 @@ it('Can add item: addItemToCart', function(){
 });
 
 it('Calculates current total: currentTotal', function(){
-    checkout.addItemPrice('a', 1);
     checkout.addItemToCart('a');
     expect(checkout.currentTotal()).to.equal(1);
 });
 
 it('Can add multiple items and get correct total', function(){
-    checkout.addItemPrice('a', 1);
-    checkout.addItemPrice('b', 2);
     checkout.addItemToCart('a');
     checkout.addItemToCart('b');
     expect(checkout.currentTotal()).to.equal(3);

859a7949c4622a68e7d27d7792c8a1007951bb55
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 17:18:42 -0400

    works

--- test/checkout.js
+++ test/checkout.js
@@ -1,11 +1,19 @@
 module.exports = class Checkout{
+    constructor(){
+        this.prices = new Object();
+        this.total = 0;
+    }
 
     addItemPrice(item, price){
-
+        this.prices[item] = price;
     }
 
     addItemToCart(item){
+        this.total += this.prices[item];
+    }
 
+    currentTotal(){
+        return this.total;
     }
 
 }

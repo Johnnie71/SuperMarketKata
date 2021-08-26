fac86e463e28ac172fa15af390ebca47ab86eea2
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 16:30:51 -0400

    deleted first test since it passes green

--- test/checkout_spec.js
+++ test/checkout_spec.js
@@ -1,10 +1,6 @@
 const Checkout = require('./checkout');
 const expect = require('chai').expect;
 
-it('Can initiate checkout class', function(){
-    let checkout = new Checkout();
-});
-
 it('Can add item price', function(){
     let checkout = new Checkout();
     checkout.addItemPrice('a', 1);

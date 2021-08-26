4e2a54db627635e786fde05d046d92cc7e773991
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 16:35:35 -0400

    add item test

--- test/checkout_spec.js
+++ test/checkout_spec.js
@@ -4,4 +4,9 @@ const expect = require('chai').expect;
 it('Can add item price', function(){
     let checkout = new Checkout();
     checkout.addItemPrice('a', 1);
+});
+
+it('Can add item', function(){
+    let checkout = new Checkout();
+    checkout.addItem();
 })

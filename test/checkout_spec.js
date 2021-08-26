8af2c1b3ed4775d0a38a029ace0a1d1c3c473fe7
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-26 16:22:19 -0400

    checking to see if item price can be added

--- test/checkout_spec.js
+++ test/checkout_spec.js
@@ -3,4 +3,9 @@ const expect = require('chai').expect;
 
 it('Can initiate checkout class', function(){
     let checkout = new Checkout();
+});
+
+it('Can add item price', function(){
+    let checkout = new Checkout();
+    checkout.addItemPrice();
 })
